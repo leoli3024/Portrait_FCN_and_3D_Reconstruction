eb6b8abe94393cd3c25f07af257de039163c2243
14leeyuchieh <14leeyuchieh@berkeley.edu>
2018-04-05 23:49:22 -0700

    need to wait for instructor response

--- fcn.py
+++ fcn.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 import skimage.io as io
 import numpy as np
 import scipy
+import cv2
+import dlib
 sys.path.append('/Users/yu-chieh/seg_models/models/slim/')
 sys.path.append("/Users/yu-chieh/seg_models/tf-image-segmentation/")
 from tf_image_segmentation.models.fcn_8s import FCN_8s
@@ -15,7 +17,46 @@ slim = tf.contrib.slim
 cpstandard = "/Users/yu-chieh/Downloads/fcn_8s_checkpoint/model_fcn8s_final.ckpt"
 
 
-def make_fcn_featurizer(batch_size, x, train_fcn=False, checkpoint_path=cpstandard):
+def get_all_images_for_fcn(num_images):
+    # get num_images images form the path and put as a matrix
+    imgs = []
+    num = 0
+    path = '/Users/yu-chieh/Downloads/images_data_crop/'
+    for f in os.listdir(path):
+        if num >= num_images:
+            return np.array(imgs)
+        image_path = os.path.join(path,f)
+        image = scipy.ndimage.imread(image_path, mode='RGB')
+        # cheating version
+        # image = np.dstack((image, get_xy_mask(image)))
+        imgs.append(image)
+        num += 1
+    return np.array(imgs)
+
+def get_facial_points(image, num_points):
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    detector = dlib.get_frontal_face_detector()
+    dets = detector(image, 1)
+    points = []
+    for k, d in enumerate(dets):
+        # Get the landmarks/parts for the face in box d.
+        shape = predictor(image, d)
+        for i in range(num_points):
+            pt = shape.part(i)
+            points.append([int(pt.x), int(pt.y)])
+    return np.array(points)
+
+def get_xy_mask(image):
+    # bad version
+    image_src = image
+    mask_dst = scipy.ndimage.imread('/Users/yu-chieh/Downloads/images_data_crop/02457.jpg', mode='RGB')
+    dst = get_facial_points(mask_dst, 30)
+    src = get_facial_points(image_src, 30)
+    h, status = cv2.findHomography(src, dst)
+    im_dst = cv2.warpPerspective(image_src, h, (image_src.shape[1], image_src.shape[0]))
+    return im_dst
+
+def test_fcn_featurizer(test_size, x, train_fcn=False, checkpoint_path=cpstandard):
     """
     ========== Args ==========
       checkpoint_path: Str. Path to `.npy` file containing AlexNet parameters.
@@ -32,37 +73,34 @@ def make_fcn_featurizer(batch_size, x, train_fcn=False, checkpoint_path=cpstanda
     num_class=21
     num_channels=3
     image_shape = (None, None, num_channels)  # RGB + Segmentation id
-    images = tf.placeholder(tf.uint8, shape=(batch_size,) + image_shape)
+    images = tf.placeholder(tf.uint8, shape=(test_size,) + image_shape)
     preprocessed_images = tf.image.resize_images(images, size=(229, 229))
 
-    # Be careful: after adaptation, network returns final labels
-    # and not logits
+    # # Be careful: after adaptation, network returns final labels
+    # # and not logits
+    # with tf.variable_scope("conv_to_channel3"):
+    #     filter_m = tf.Variable(tf.random_normal([1,1,num_channels,3]))
+    #     preprocessed_images_3_channels = tf.nn.conv2d(preprocessed_images, filter_m, strides=[1, 1, 1, 1], padding='VALID')
+    #     shape_of_this = tf.shape(preprocessed_images_3_channels)
+
     model = adapt_network_for_any_size_input(FCN_8s, size_muliple)
-    
     pred, fcn_16s_variables_mapping = model(image_batch_tensor=preprocessed_images,
                                           number_of_classes=num_class,
                                           is_training=train_fcn)
-
-    
-
-
+    binary_pred = tf.nn.sigmoid(tf.cast(pred, tf.float32), name="sigmoid")
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         # restore checkpoint
         saver = tf.train.Saver()
         saver.restore(sess, checkpoint_path)
-        original_imgs, output_masks = sess.run([images, pred], feed_dict={images: x})
+        # a = sess.run([shape_of_this], feed_dict={images: x})
+        # print(a)
+        original_imgs, output_masks = sess.run([images, binary_pred], feed_dict={images: x})
         io.imshow(original_imgs[0])
         io.show()
-        
         io.imshow(output_masks[0].squeeze())
         io.show()
 
-
-image = scipy.ndimage.imread('/Users/yu-chieh/Desktop/Sierra.png', mode='RGB')
-image = np.resize(image, (1,) + image.shape)
-print(image.shape)
-
-make_fcn_featurizer(1, image)
-# make_fcn_featurizer(1, np.zeros((1, 229, 229, 3)))
-
+imgs = get_all_images_for_fcn(2)
+print(imgs.shape)
+test_fcn_featurizer(2, imgs)

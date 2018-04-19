6493234304c36b5f8212bf30846bd1a9e93b5f5b
14leeyuchieh <14leeyuchieh@berkeley.edu>
2018-04-19 10:55:45 -0700

    train without applying transforms

--- portrait_plus.py
+++ portrait_plus.py
@@ -4,6 +4,7 @@ import os
 from PIL import Image
 import math
 from scipy import misc
+import matplotlib.pyplot as plt
 
 class BatchDatset:
     imgs = []
@@ -110,108 +111,111 @@ class BatchDatset:
             imgs.append(nimat)
             labels.append(namat)
 
-            angs = [-45, -22, 22, 45]
-            gammas = [0.5, 0.8, 1.2, 1.5]
-            scales = [0.6, 0.8, 1.2, 1.5]
-            h, w, _ = nimat.shape
-            org_mat = np.zeros((h, w, 3), dtype=np.int)
-            app_mat = np.zeros((h, w, 3), dtype=np.int)
-            min3 = np.min(nimat[:, :, 3])
-            min4 = np.min(nimat[:, :, 4])
-            min5 = np.min(nimat[:, :, 5])
-            ran3 = np.max(nimat[:, :, 3]) - min3
-            ran4 = np.max(nimat[:, :, 4]) - min4
-            ran5 = np.max(nimat[:, :, 5]) - min5
-            org_mat[:, :, 0] = np.round(nimat[:, :, 2] * 255 + 122.675)
-            org_mat[:, :, 1] = np.round(nimat[:, :, 1] * 255 + 116.669)
-            org_mat[:, :, 2] = np.round(nimat[:, :, 0] * 255 + 104.008)
-            if ran3 != 0:
-                app_mat[:, :, 0] = np.round((nimat[:, :, 3] - min3) * 255 / ran3)
-            else:
-                app_mat[:, :, 0] = min3
-            if ran4 != 0:
-                app_mat[:, :, 1] = np.round((nimat[:, :, 4] - min4) * 255 / ran4)
-            else:
-                app_mat[:, :, 0] = min4
-            if ran5 != 0:
-                app_mat[:, :, 2] = np.round((nimat[:, :, 5] - min5) * 255 / ran5)
-            else:
-                app_mat[:, :, 0] = min5
-            i_img = Image.fromarray(np.uint8(org_mat))
-            p_img = Image.fromarray(np.uint8(app_mat))
-            a_img = Image.fromarray(np.uint8(amat))
-            for i in range(4):
-                # rotation
-                tmpi_img = i_img.rotate(angs[i])
-                tmpp_img = p_img.rotate(angs[i])
-                tmpa_img = a_img.rotate(angs[i])
-                tmpri_img = np.array(tmpi_img, dtype=np.int)
-                tmprp_img = np.array(tmpp_img, dtype=np.int)
-                rot_p1, rot_p2 = self.rotateNormalizedCord(nimat[:, :, 3], nimat[:, :, 4], angs[i] * math.pi / 180)
-                rimat = np.zeros((h, w, 6), dtype=np.float)
-                rimat[:, :, 0] = (tmpri_img[:, :, 2] - 104.008) / 255
-                rimat[:, :, 1] = (tmpri_img[:, :, 1] - 116.669) / 255
-                rimat[:, :, 2] = (tmpri_img[:, :, 0] - 122.675) / 255
-                rimat[:, :, 3] = rot_p1
-                rimat[:, :, 4] = rot_p2
-                rimat[:, :, 5] = tmprp_img[:, :, 2] * ran5 / 255 + min5
-                imgs.append(rimat)
-                labels.append(np.array(tmpa_img, dtype=np.int))
-                # gamma transformation
-                tmp_nimat = np.array(imat, dtype=np.float)
-                tmp_nimat[:, :, 0] = tmp_nimat[:, :, 0] + 104.008 / 255
-                tmp_nimat[:, :, 0] = (pow(tmp_nimat[:, :, 0], gammas[i]) - pow(104.008 / 255, gammas[i]))
-                tmp_nimat[:, :, 1] = tmp_nimat[:, :, 1] + 116.669 / 255
-                tmp_nimat[:, :, 1] = (pow(tmp_nimat[:, :, 1], gammas[i]) - pow(116.669 / 255, gammas[i]))
-                tmp_nimat[:, :, 2] = tmp_nimat[:, :, 2] + 122.675 / 255
-                tmp_nimat[:, :, 2] = (pow(tmp_nimat[:, :, 2], gammas[i]) - pow(122.675 / 255, gammas[i]))
-                imgs.append(tmp_nimat)
-                labels.append(namat)
-                # scale transformation
-                if scales[i] > 1.0:
-                    resize_box = (int(round(scales[i] * w)), int(round(scales[i] * h)))
-                    si_img = i_img.resize(resize_box, Image.ANTIALIAS)
-                    sp_img = p_img.resize(resize_box, Image.ANTIALIAS)
-                    sa_img = a_img.resize(resize_box, Image.ANTIALIAS)
-                    crop_up, crop_down = (scales[i] - 1) / 2, (scales[i] + 1) / 2
-                    crop_box = (int(round(crop_up * w)), int(round(crop_up * h)), int(round(crop_down * w)), int(round(crop_down * h)))
-                    ci_img = si_img.crop(crop_box)
-                    cp_img = sp_img.crop(crop_box)
-                    ca_img = sa_img.crop(crop_box)
-                    tmpsi_img = np.array(ci_img, dtype=np.int)
-                    tmpsp_img = np.array(cp_img, dtype=np.int)
-                    tmpsa_img = np.array(ca_img, dtype=np.int)
-                    simat = np.zeros(imat.shape, dtype=np.float)
-                    simat[:, :, 0] = (tmpsi_img[:, :, 2] - 104.008) / 255
-                    simat[:, :, 1] = (tmpsi_img[:, :, 1] - 116.669) / 255
-                    simat[:, :, 2] = (tmpsi_img[:, :, 0] - 122.675) / 255
-                    xmat, ymat = self.scaleNormalizedCord(nimat[:, :, 3], nimat[:, :, 4], scales[i] * 300)
-                    simat[:, :, 3] = xmat
-                    simat[:, :, 4] = ymat
-                    simat[:, :, 5] = tmpsp_img[:, :, 2] * ran5 / 255 + min5
-                    imgs.append(simat)
-                    labels.append(tmpsa_img)
-                else:
-                    resize_box = (int(round(scales[i] * w)), int(round(scales[i] * h)))
-                    si_img = i_img.resize(resize_box, Image.ANTIALIAS)
-                    sp_img = p_img.resize(resize_box, Image.ANTIALIAS)
-                    sa_img = a_img.resize(resize_box, Image.ANTIALIAS)
-                    tmpsi_img = np.array(si_img, dtype=np.int)
-                    tmpsp_img = np.array(sp_img, dtype=np.int)
-                    tmpsa_img = np.array(sa_img, dtype=np.int)
-                    simat = np.zeros(imat.shape, dtype=np.float)
-                    samat = np.zeros(amat.shape, dtype=np.int)
-                    crop_up, crop_down = (1 - scales[i]) / 2, (1 + scales[i]) / 2
-                    simat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w)), 0] = (tmpsi_img[:, :, 2] - 104.008) / 255
-                    simat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w)), 1] = (tmpsi_img[:, :, 1] - 116.669) / 255
-                    simat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w)), 2] = (tmpsi_img[:, :, 0] - 122.675) / 255
-                    simat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w)), 5] = tmpsp_img[:, :, 2] * ran5 / 255 + min5
-                    samat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w))] = tmpsa_img
-                    xmat, ymat = self.scaleNormalizedCord(nimat[:, :, 3], nimat[:, :, 4], scales[i] * 300)
-                    simat[:, :, 3] = xmat
-                    simat[:, :, 4] = ymat
-                    imgs.append(simat)
-                    labels.append(samat)
+        #     angs = [-45, -22, 22, 45]
+        #     gammas = [0.5, 0.8, 1.2, 1.5]
+        #     scales = [0.6, 0.8, 1.2, 1.5]
+        #     h, w, _ = nimat.shape
+        #     org_mat = np.zeros((h, w, 3), dtype=np.int)
+        #     app_mat = np.zeros((h, w, 3), dtype=np.int)
+        #     min3 = np.min(nimat[:, :, 3])
+        #     min4 = np.min(nimat[:, :, 4])
+        #     min5 = np.min(nimat[:, :, 5])
+        #     ran3 = np.max(nimat[:, :, 3]) - min3
+        #     ran4 = np.max(nimat[:, :, 4]) - min4
+        #     ran5 = np.max(nimat[:, :, 5]) - min5
+        #     org_mat[:, :, 0] = np.round(nimat[:, :, 2] * 255 + 122.675)
+        #     org_mat[:, :, 1] = np.round(nimat[:, :, 1] * 255 + 116.669)
+        #     org_mat[:, :, 2] = np.round(nimat[:, :, 0] * 255 + 104.008)
+        #     if ran3 != 0:
+        #         app_mat[:, :, 0] = np.round((nimat[:, :, 3] - min3) * 255 / ran3)
+        #     else:
+        #         app_mat[:, :, 0] = min3
+        #     if ran4 != 0:
+        #         app_mat[:, :, 1] = np.round((nimat[:, :, 4] - min4) * 255 / ran4)
+        #     else:
+        #         app_mat[:, :, 0] = min4
+        #     if ran5 != 0:
+        #         app_mat[:, :, 2] = np.round((nimat[:, :, 5] - min5) * 255 / ran5)
+        #     else:
+        #         app_mat[:, :, 0] = min5
+        #     i_img = Image.fromarray(np.uint8(org_mat))
+        #     p_img = Image.fromarray(np.uint8(app_mat))
+        #     a_img = Image.fromarray(np.uint8(amat))
+        #     for i in range(4):
+        #         # rotation
+        #         tmpi_img = i_img.rotate(angs[i])
+        #         tmpp_img = p_img.rotate(angs[i])
+        #         tmpa_img = a_img.rotate(angs[i])
+        #         tmpri_img = np.array(tmpi_img, dtype=np.int)
+        #         tmprp_img = np.array(tmpp_img, dtype=np.int)
+        #         rot_p1, rot_p2 = self.rotateNormalizedCord(nimat[:, :, 3], nimat[:, :, 4], angs[i] * math.pi / 180)
+        #         rimat = np.zeros((h, w, 6), dtype=np.float)
+        #         rimat[:, :, 0] = (tmpri_img[:, :, 2] - 104.008) / 255
+        #         rimat[:, :, 1] = (tmpri_img[:, :, 1] - 116.669) / 255
+        #         rimat[:, :, 2] = (tmpri_img[:, :, 0] - 122.675) / 255
+        #         rimat[:, :, 3] = rot_p1
+        #         rimat[:, :, 4] = rot_p2
+        #         rimat[:, :, 5] = tmprp_img[:, :, 2] * ran5 / 255 + min5
+        #         imgs.append(rimat)
+        #         labels.append(np.array(tmpa_img, dtype=np.int))
+        #         # gamma transformation
+        #         tmp_nimat = np.array(imat, dtype=np.float)
+        #         tmp_nimat[:, :, 0] = tmp_nimat[:, :, 0] + 104.008 / 255
+        #         tmp_nimat[:, :, 0] = (pow(tmp_nimat[:, :, 0], gammas[i]) - pow(104.008 / 255, gammas[i]))
+        #         tmp_nimat[:, :, 1] = tmp_nimat[:, :, 1] + 116.669 / 255
+        #         tmp_nimat[:, :, 1] = (pow(tmp_nimat[:, :, 1], gammas[i]) - pow(116.669 / 255, gammas[i]))
+        #         tmp_nimat[:, :, 2] = tmp_nimat[:, :, 2] + 122.675 / 255
+        #         tmp_nimat[:, :, 2] = (pow(tmp_nimat[:, :, 2], gammas[i]) - pow(122.675 / 255, gammas[i]))
+        #         imgs.append(tmp_nimat)
+        #         labels.append(namat)
+        #         # scale transformation
+        #         if scales[i] > 1.0:
+        #             resize_box = (int(round(scales[i] * w)), int(round(scales[i] * h)))
+        #             si_img = i_img.resize(resize_box, Image.ANTIALIAS)
+        #             sp_img = p_img.resize(resize_box, Image.ANTIALIAS)
+        #             sa_img = a_img.resize(resize_box, Image.ANTIALIAS)
+        #             crop_up, crop_down = (scales[i] - 1) / 2, (scales[i] + 1) / 2
+        #             crop_box = (int(round(crop_up * w)), int(round(crop_up * h)), int(round(crop_down * w)), int(round(crop_down * h)))
+        #             ci_img = si_img.crop(crop_box)
+        #             cp_img = sp_img.crop(crop_box)
+        #             ca_img = sa_img.crop(crop_box)
+        #             tmpsi_img = np.array(ci_img, dtype=np.int)
+        #             tmpsp_img = np.array(cp_img, dtype=np.int)
+        #             tmpsa_img = np.array(ca_img, dtype=np.int)
+        #             simat = np.zeros(imat.shape, dtype=np.float)
+        #             simat[:, :, 0] = (tmpsi_img[:, :, 2] - 104.008) / 255
+        #             simat[:, :, 1] = (tmpsi_img[:, :, 1] - 116.669) / 255
+        #             simat[:, :, 2] = (tmpsi_img[:, :, 0] - 122.675) / 255
+        #             xmat, ymat = self.scaleNormalizedCord(nimat[:, :, 3], nimat[:, :, 4], scales[i] * 300)
+        #             simat[:, :, 3] = xmat
+        #             simat[:, :, 4] = ymat
+        #             simat[:, :, 5] = tmpsp_img[:, :, 2] * ran5 / 255 + min5
+        #             imgs.append(simat)
+        #             labels.append(tmpsa_img)
+        #         else:
+        #             resize_box = (int(round(scales[i] * w)), int(round(scales[i] * h)))
+        #             si_img = i_img.resize(resize_box, Image.ANTIALIAS)
+        #             sp_img = p_img.resize(resize_box, Image.ANTIALIAS)
+        #             sa_img = a_img.resize(resize_box, Image.ANTIALIAS)
+        #             tmpsi_img = np.array(si_img, dtype=np.int)
+        #             tmpsp_img = np.array(sp_img, dtype=np.int)
+        #             tmpsa_img = np.array(sa_img, dtype=np.int)
+        #             simat = np.zeros(imat.shape, dtype=np.float)
+        #             samat = np.zeros(amat.shape, dtype=np.int)
+        #             crop_up, crop_down = (1 - scales[i]) / 2, (1 + scales[i]) / 2
+        #             simat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w)), 0] = (tmpsi_img[:, :, 2] - 104.008) / 255
+        #             simat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w)), 1] = (tmpsi_img[:, :, 1] - 116.669) / 255
+        #             simat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w)), 2] = (tmpsi_img[:, :, 0] - 122.675) / 255
+        #             simat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w)), 5] = tmpsp_img[:, :, 2] * ran5 / 255 + min5
+        #             samat[int(round(crop_up * h)):int(round(crop_down * h)), int(round(crop_up * w)):int(round(crop_down * w))] = tmpsa_img
+        #             xmat, ymat = self.scaleNormalizedCord(nimat[:, :, 3], nimat[:, :, 4], scales[i] * 300)
+        #             simat[:, :, 3] = xmat
+        #             simat[:, :, 4] = ymat
+        #             imgs.append(simat)
+        #             labels.append(samat)
+        # # for i in imgs:
+        # #     plt.imshow(i[:, :, 3])
+        # #     plt.show()
         return imgs, labels
 
 
@@ -290,25 +294,28 @@ class TestDataset:
         return None, None, None
 
 
-if __name__ == '__main__':
-    dat = BatchDatset('data/trainlist.mat', batch_size=13)
-    rimat, ramat = dat.next_batch()
-    for i in range(13):
-        imat = rimat[i]
-        amat = ramat[i]
-        rgb = np.zeros((imat.shape[0], imat.shape[1], 3), dtype=np.int)
-        rgb[:, :, 0] = np.round(imat[:, :, 2] * 255 + 122.675)
-        rgb[:, :, 1] = np.round(imat[:, :, 1] * 255 + 116.669)
-        rgb[:, :, 2] = np.round(imat[:, :, 0] * 255 + 104.008)
-        misc.imsave('res/org' + str(i) + '.jpg', rgb)
-        xxc = imat[:, :, 3]
-        xxc = np.round((xxc - np.min(xxc) / (np.max(xxc) - np.min(xxc))) * 255)
-        misc.imsave('res/xxc' + str(i) + '.jpg', xxc)
-        yyc = imat[:, :, 4]
-        yyc = np.round((yyc - np.min(yyc) / (np.max(yyc) - np.min(yyc))) * 255)
-        misc.imsave('res/yyc' + str(i) + '.jpg', yyc)
-        mean = imat[:, :, 5] * 255
-        misc.imsave('res/mean' + str(i) + '.jpg', mean)
-        alpha = amat * 255
-        alpha = alpha.reshape((alpha.shape[0], alpha.shape[1]))
-        misc.imsave('res/alpha' + str(i) + '.jpg', alpha)
+# if __name__ == '__main__':
+    # dat = BatchDatset('data/trainlist.mat', batch_size=5)
+    # rimat, ramat = dat.next_batch()
+    # for i in range(len(rimat)):
+    #     imat = rimat[i]
+    #     amat = ramat[i]
+    #     rgb = np.zeros((imat.shape[0], imat.shape[1], 3), dtype=np.int)
+    #     rgb[:, :, 0] = np.round(imat[:, :, 2] * 255 + 122.675)
+    #     rgb[:, :, 1] = np.round(imat[:, :, 1] * 255 + 116.669)
+    #     rgb[:, :, 2] = np.round(imat[:, :, 0] * 255 + 104.008)
+    #     misc.imsave('res/org' + str(i) + '.jpg', rgb)
+    #     xxc = imat[:, :, 3]
+    #     xxc = np.round((xxc - np.min(xxc) / (np.max(xxc) - np.min(xxc))) * 255)
+    #     misc.imsave('res/xxc' + str(i) + '.jpg', xxc)
+    #     yyc = imat[:, :, 4]
+    #     yyc = np.round((yyc - np.min(yyc) / (np.max(yyc) - np.min(yyc))) * 255)
+    #     misc.imsave('res/yyc' + str(i) + '.jpg', yyc)
+    #     mean = imat[:, :, 5] * 255
+    #     misc.imsave('res/mean' + str(i) + '.jpg', mean)
+    #     alpha = amat * 255
+    #     alpha = alpha.reshape((alpha.shape[0], alpha.shape[1]))
+    #     print(set(alpha.flatten()))
+    #     misc.imsave('res/alpha' + str(i) + '.jpg', alpha)
+    # # train_dataset_reader = BatchDatset('data/trainlist.mat', "train", 1)
+    # # train_images, train_annotations = train_dataset_reader.next_batch()
